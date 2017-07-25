2b06a4504e0a656a70e151c4e9a5717e9bc9909d
Mkdocs tool <mrthinlt@gmail.com>
2017-07-25 04:26:38 +0000

    Build documents

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,1 +1,19 @@
-
+/*jQuery function*/
+$(document).ready(function(){
+    $(".fa-search").click(function(){
+        $(this).hide();
+        $(".fa-search-close").show();
+        $(".navbar-default .navbar-left").hide();
+        $("#form-search").fadeIn();
+        $("#mkdocs-search-results").fadeIn();
+        $(".search-backdrop").show();
+    });
+    $(".fa-search-close").click(function(){
+        $(this).hide();
+        $(".fa-search").show();
+        $(".navbar-default .navbar-left").show();
+        $("#form-search").hide();
+        $("#mkdocs-search-results").fadeOut();
+        $(".search-backdrop").fadeOut();
+    });
+});
