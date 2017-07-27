9a436d05fcd7ec142c92342d5a44014162542304
Mkdocs tool <mrthinlt@gmail.com>
2017-07-27 02:03:39 +0000

    Build documents

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -18,4 +18,44 @@ $(document).ready(function(){
         $("#mkdocs-search-results").fadeOut();
         $(".search-backdrop").fadeOut();
     });
+
+    //Scroll app
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('.bs-sidenav a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('.bs-sidenav li').each(function () {
+            $(this).removeClass('active');
+        })
+        $('.bs-sidenav li').addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('ul.bs-sidenav li > a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('ul.bs-sidenav li').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            currLink.parent().removeClass("active");
+        }
+    });
+}
